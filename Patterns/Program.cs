819cd522b4e54d04965adf976b4d80a87247a970
dima tkachenko <dtkachenko06@gmail.com>
2023-11-28 14:11:50 +0200

    Singelton

--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -1,95 +1,47 @@
 ﻿using System;
 
-public class EnemeyTrait
+public sealed class LoadBalancer
 {
-    public int BoostTime { get; set; }
-    public int BoostDamage { get; set; }
-}
+    private static readonly Lazy<LoadBalancer> _instance = new(() => new LoadBalancer());
+    private bool _isOnline;
 
-public abstract class Enemy : ICloneable
-{
-    private int m_Health;
-    private int m_Attack;
-    private double m_Damage;
-    private EnemeyTrait m_EnemyTrait = new EnemeyTrait();
-
-    public int Health
+    private LoadBalancer()
     {
-        get { return m_Health; }
-        set { m_Health = value; }
+        // Ініціалізація класу
+        _isOnline = false;
     }
 
-    public int Attack
+    public static LoadBalancer Instance
     {
-        get { return m_Attack; }
-        set { m_Attack = value; }
+        get { return _instance.Value; }
     }
 
-    public double Damage
+    public bool IsOnline
     {
-        get { return m_Damage; }
-        set { m_Damage = value; }
+        get { return _isOnline; }
     }
 
-    public EnemeyTrait EnemyTrait
+    public void SetOnlineStatus(bool isOnline)
     {
-        get { return m_EnemyTrait; }
-        set { m_EnemyTrait = value; }
-    }
-
-    public virtual object Clone()
-    {
-        // Choose whether to perform a shallow or deep copy
-        // return ShallowCopy();
-        return DeepCopy();
-    }
-
-    private object ShallowCopy()
-    {
-        return this.MemberwiseClone();
-    }
-
-    private object DeepCopy()
-    {
-        Enemy cloned = this.MemberwiseClone() as Enemy;
-        cloned.EnemyTrait = new EnemeyTrait
-        {
-            BoostDamage = this.EnemyTrait.BoostDamage,
-            BoostTime = this.EnemyTrait.BoostTime
-        };
-
-        return cloned;
-    }
-}
-public class Elephant : Enemy
-{
-    public override Enemy Clone()
-    {
-        return this.MemberwiseClone() as Elephant;
+        _isOnline = isOnline;
+        Console.WriteLine($"Web server is {(isOnline ? "online" : "offline")}");
     }
 }
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        // The code to demonstrate the classic Prototype Pattern
-        Elephant elephant = new Elephant
-        {
-            Health = 100,
-            Attack = 10,
-            Damage = 5.0,
-            EnemyTrait = { BoostTime = 7, BoostDamage = 7 }
-        };
+        // Клієнтський код
+        LoadBalancer loadBalancer1 = LoadBalancer.Instance;
+        LoadBalancer loadBalancer2 = LoadBalancer.Instance;
 
-        Elephant playerToSave = elephant.Clone() as Elephant;
-        playerToSave.EnemyTrait.BoostTime = 10;
-        playerToSave.EnemyTrait.BoostDamage = 10;
+        Console.WriteLine($"Are both instances the same? {loadBalancer1 == loadBalancer2}");
 
-        Console.WriteLine("Original Enemy stats:");
-        Console.WriteLine($"BoostTime: {elephant.EnemyTrait.BoostTime}, BoostDamage: {elephant.EnemyTrait.BoostDamage}");
+        // Зміна статусу сервера через будь-який екземпляр класу
+        loadBalancer1.SetOnlineStatus(true);
 
-        Console.WriteLine("\nCopied Object Value:");
-        Console.WriteLine($"BoostTime: {playerToSave.EnemyTrait.BoostTime}, BoostDamage: {playerToSave.EnemyTrait.BoostDamage}");
+        // Перевірка статусу через інший екземпляр класу
+        Console.WriteLine($"Is the server online? {loadBalancer2.IsOnline}");
     }
 }
