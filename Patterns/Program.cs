41f33910cfc352f37c1fb783ad71e9c21077f6de
dima tkachenko <dtkachenko06@gmail.com>
2023-12-11 13:05:01 +0200

    Decorator

--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -1,131 +1,87 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Drawing;
-using System.Linq;
+using System.Text;
 
-public class Point
+public class CodeBuilder
 {
-    public int X;
-    public int Y;
+    private StringBuilder builder = new StringBuilder();
+    private int indentLevel = 0;
 
-    public Point(int x, int y)
+    public CodeBuilder Indent()
     {
-        X = x;
-        Y = y;
-    }
-}
-
-public class Line
-{
-    public Point Start, End;
-
-    public Line(Point start, Point end)
-    {
-        Start = start;
-        End = end;
-    }
-}
-
-public interface IDrawable
-{
-    void DrawPoint(Point p);
-}
-
-public abstract class VectorObject : Collection<Line>, IDrawable
-{
-    public abstract void Draw();
-    public abstract void DrawPoint(Point p);
-}
-
-public class VectorRectangle : VectorObject
-{
-    public VectorRectangle(int x, int y, int width, int height)
-    {
-        Add(new Line(new Point(x, y), new Point(x + width, y)));
-        Add(new Line(new Point(x + width, y), new Point(x + width, y + height)));
-        Add(new Line(new Point(x, y), new Point(x, y + height)));
-        Add(new Line(new Point(x, y + height), new Point(x + width, y + height)));
+        indentLevel++;
+        return this;
     }
 
-    public override void Draw()
+    public CodeBuilder Outdent()
     {
-        foreach (var line in this)
+        if (indentLevel > 0)
         {
-            Console.WriteLine($"Drawing line from ({line.Start.X}, {line.Start.Y}) to ({line.End.X}, {line.End.Y})");
+            indentLevel--;
         }
+        return this;
     }
 
-    public override void DrawPoint(Point p)
+    public CodeBuilder Append(string value)
     {
-        Console.WriteLine($"Drawing point at ({p.X}, {p.Y})");
-    }
-}
-
-public class LineToPointAdapter : Collection<Point>
-{
-    private static int count = 0;
-
-    public LineToPointAdapter(Line line)
-    {
-        Console.WriteLine($"{++count}: Generating points for line " +
-            $"[{line.Start.X},{line.Start.Y}]-[{line.End.X},{line.End.Y}] (no caching)");
-
-        int left = Math.Min(line.Start.X, line.End.X);
-        int right = Math.Max(line.Start.X, line.End.X);
-        int top = Math.Min(line.Start.Y, line.End.Y);
-        int bottom = Math.Max(line.Start.Y, line.End.Y);
-
-        if (right - left == 0)
-        {
-            for (int y = top; y <= bottom; ++y)
-            {
-                Add(new Point(left, y));
-            }
-        }
-        else if (line.End.Y - line.Start.Y == 0)
-        {
-            for (int x = left; x <= right; ++x)
-            {
-                Add(new Point(x, top));
-            }
-        }
-    }
-}
-
-public static class GraphicEditor
-{
-    private static readonly List<VectorObject> vectorObjects = new List<VectorObject>
-    {
-        new VectorRectangle(1, 1, 10, 10),
-        new VectorRectangle(3, 3, 6, 6)
-    };
-
-    public static void DrawPoint(Point p)
-    {
-        Console.Write(".");
+        builder.Append(new string(' ', 4 * indentLevel));
+        builder.Append(value);
+        return this;
     }
 
-    public static void Draw()
+    public CodeBuilder Append(char value, int repeatCount)
     {
-        foreach (var vo in vectorObjects)
-        {
-            foreach (var line in vo)
-            {
-                var adapter = new LineToPointAdapter(line);
-                foreach (var point in adapter)
-                {
-                    DrawPoint(point);
-                }
-            }
-        }
+        builder.Append(value, repeatCount);
+        return this;
+    }
+
+    public CodeBuilder AppendLine()
+    {
+        builder.AppendLine();
+        return this;
+    }
+
+    public CodeBuilder AppendLine(string value)
+    {
+        builder.Append(new string(' ', 4 * indentLevel));
+        builder.AppendLine(value);
+        return this;
+    }
+
+    public override string ToString()
+    {
+        return builder.ToString();
     }
 }
 
 class Program
 {
+    public static void PrintTimeStamp(string name)
+    {
+        DateTime localDate = DateTime.Now;
+        var culture = new System.Globalization.CultureInfo("ua-UA");
+        Console.WriteLine("Дата та час компіляції: {0}", localDate.ToString(culture));
+        Console.WriteLine("Автор програми: {0}", name);
+    }
+
     static void Main()
     {
-        GraphicEditor.Draw();
+        CodeBuilder myBuilder = new CodeBuilder();
+        myBuilder
+            .Append("public class HelloWorld")
+            .AppendLine()
+            .AppendLine("{")
+            .Indent()
+            .AppendLine("public static void Main(string[] args)")
+            .AppendLine("{")
+            .Indent()
+            .AppendLine("Console.WriteLine(\"Hello, World\");")
+            .Outdent()
+            .AppendLine("}")
+            .Outdent()
+            .AppendLine("}");
+
+        Console.WriteLine(myBuilder.ToString());
+
+        PrintTimeStamp("Лісун Іван, студент групи 2П-21");
     }
 }
