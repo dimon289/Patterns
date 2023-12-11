4dd4085c5931b04ea0df13129cba22b2f85208a4
dima tkachenko <dtkachenko06@gmail.com>
2023-12-11 13:09:54 +0200

    facade

--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -1,87 +1,106 @@
 ﻿using System;
-using System.Text;
+using System.Collections.Generic;
+using System.Linq;
 
-public class CodeBuilder
+public struct Point
 {
-    private StringBuilder builder = new StringBuilder();
-    private int indentLevel = 0;
+    public int X { get; set; }
+    public int Y { get; set; }
 
-    public CodeBuilder Indent()
+    public Point(int x, int y)
     {
-        indentLevel++;
-        return this;
-    }
-
-    public CodeBuilder Outdent()
-    {
-        if (indentLevel > 0)
-        {
-            indentLevel--;
-        }
-        return this;
-    }
-
-    public CodeBuilder Append(string value)
-    {
-        builder.Append(new string(' ', 4 * indentLevel));
-        builder.Append(value);
-        return this;
-    }
-
-    public CodeBuilder Append(char value, int repeatCount)
-    {
-        builder.Append(value, repeatCount);
-        return this;
-    }
-
-    public CodeBuilder AppendLine()
-    {
-        builder.AppendLine();
-        return this;
-    }
-
-    public CodeBuilder AppendLine(string value)
-    {
-        builder.Append(new string(' ', 4 * indentLevel));
-        builder.AppendLine(value);
-        return this;
-    }
-
-    public override string ToString()
-    {
-        return builder.ToString();
+        X = x;
+        Y = y;
     }
 }
 
+public interface IBuffer
+{
+    void Write(string value);
+    void WriteLine(string value);
+    string ReadLine();
+    // Other methods and properties as needed
+}
+
+public class TableBuffer : IBuffer
+{
+    private readonly TableColumnSpec[] spec;
+    private readonly int totalHeight;
+    private readonly List<string[]> buffer;
+    private static readonly Point invalidPoint = new Point(-1, -1);
+    private readonly short[,] formatBuffer;
+
+    public TableBuffer(TableColumnSpec[] spec, int totalHeight)
+    {
+        this.spec = spec;
+        this.totalHeight = totalHeight;
+        buffer = new List<string[]>();
+        for (int i = 0; i < (totalHeight - 1); ++i)
+        {
+            buffer.Add(new string[spec.Length]);
+        }
+        formatBuffer = new short[spec.Max(s => s.Width), totalHeight];
+    }
+
+    public void Write(string value)
+    {
+        // Implement the Write method
+    }
+
+    public void WriteLine(string value)
+    {
+        // Implement the WriteLine method
+    }
+
+    public string ReadLine()
+    {
+        // Implement the ReadLine method
+        return string.Empty; // Replace with the actual implementation
+    }
+
+    public struct TableColumnSpec
+    {
+        public string Header;
+        public int Width;
+        public TableColumnAlignment Alignment;
+    }
+}
+
+public enum TableColumnAlignment
+{
+    Left,
+    Right,
+    Center
+}
 class Program
 {
-    public static void PrintTimeStamp(string name)
-    {
-        DateTime localDate = DateTime.Now;
-        var culture = new System.Globalization.CultureInfo("ua-UA");
-        Console.WriteLine("Дата та час компіляції: {0}", localDate.ToString(culture));
-        Console.WriteLine("Автор програми: {0}", name);
-    }
 
     static void Main()
     {
-        CodeBuilder myBuilder = new CodeBuilder();
-        myBuilder
-            .Append("public class HelloWorld")
-            .AppendLine()
-            .AppendLine("{")
-            .Indent()
-            .AppendLine("public static void Main(string[] args)")
-            .AppendLine("{")
-            .Indent()
-            .AppendLine("Console.WriteLine(\"Hello, World\");")
-            .Outdent()
-            .AppendLine("}")
-            .Outdent()
-            .AppendLine("}");
+        // Define column specifications for the table
+        var columnSpecs = new TableBuffer.TableColumnSpec[]
+        {
+        new TableBuffer.TableColumnSpec { Header = "Column1", Width = 10, Alignment = TableColumnAlignment.Left },
+        new TableBuffer.TableColumnSpec { Header = "Column2", Width = 20, Alignment = TableColumnAlignment.Right },
+        new TableBuffer.TableColumnSpec { Header = "Column3", Width = 15, Alignment = TableColumnAlignment.Center }
+        };
 
-        Console.WriteLine(myBuilder.ToString());
+        // Create a TableBuffer with the specified column specs and total height
+        var buffer = new TableBuffer(columnSpecs, 10);
 
-        PrintTimeStamp("Лісун Іван, студент групи 2П-21");
+        // Now you can use the buffer as before
+        buffer.Write("Hello, ");
+        buffer.WriteLine("World!");
+        buffer.Write("This is a ");
+        buffer.Write("TableBuffer");
+        buffer.WriteLine(" example");
+
+        string line1 = buffer.ReadLine();
+        string line2 = buffer.ReadLine();
+        string line3 = buffer.ReadLine();
+
+        Console.WriteLine($"Line 1: {line1}");
+        Console.WriteLine($"Line 2: {line2}");
+        Console.WriteLine($"Line 3: {line3}");
     }
 }
